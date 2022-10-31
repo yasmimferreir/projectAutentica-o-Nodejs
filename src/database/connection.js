e58fb01e3bddc3597efb5267150322dd890aa089
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-10-31 07:21:55 -0400

    atualização do projeto

---
+++ src/database/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+
+const database = process.env.DATABASE;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const dialect = process.env.DIALECT;
+
+const connection = new Sequelize(database, username, password, {
+    host,
+    dialect
+})
+
+module.exports = connection
